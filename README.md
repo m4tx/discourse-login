adfe69cfb6b2256a0c7d582bf5fa8a07ef720d0f
Mateusz Maćkowski <m4tx@m4tx.pl>
2018-07-08 14:23:00 +0200

    Print OK message to stdout; reformat script and update README.md

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ $EDITOR $HOME/.config/discourse-login.conf
 ```
 
 Using the editor, change the variables to the match the forum you want to visit
-every day. Make sure `DISCOURSE_URL` does not contain trailing slash, and
-`DISCOURSE_TO_VISIT` should be a relative URL to the topic you want to visit
-after signing in (ideally, this should be a topic that requires authentication
-to make sure that the user was sucessfully signed in). Now, executing 
+every day. Make sure `DISCOURSE_URL` does **not** contain trailing slash, and
+`DISCOURSE_TO_VISIT` is a relative URL to the topic you want to visit after
+signing in (ideally, this should be a topic that requires authentication
+to make sure that the user was successfully signed in). Now, executing
 `./discourse-login.sh` should give an output similar to the following:
 
 ```
@@ -27,9 +27,9 @@ Visited the website successfully at Sat Jul  7 15:19:28 CEST 2018
 ```
 
 If so, it means that everything is set up properly! You can now copy the script
-to `/usr/bin` or `~/.local/bin` if you want. You should also check that the 
-configuration file has 600/400 permissions to make sure nobody but you (in
-the OS) can read it.
+to `/usr/bin` or `~/.local/bin` if you want. You should also change the
+configuration file permissions to 600/400 permissions to make sure nobody but
+you (in the OS) can read it.
 
 ## cron
 
@@ -38,9 +38,12 @@ The simplest way to do so is to set up a cron job. Execute `crontab -e` then,
 and paste the following at the end of the file that just showed up:
 
 ```
-0 */12 * * * $HOME/.local/bin/discourse-login
+0 */12 * * * $HOME/.local/bin/discourse-login >> /var/log/discourse-login/discourse-login.log
 ``` 
 
 This will instruct cron to run the script every 12 hours. You should of course
-substitute `$HOME/.local/bin/discourse-login` for your own installation
-directory. 
+substitute `$HOME/.local/bin/discourse-login` with your own installation path,
+and create `/var/log/discourse-login/` directory (with write access for your
+local user) or change this to your favorite logging directory (possibly
+`/dev/null`). Since you are only redirecting `STDOUT` to a file, cron will still
+email you in case of any errors.
--- discourse-login.sh
+++ discourse-login.sh
@@ -13,10 +13,11 @@ if [ ! -f ${conf_path} ]; then
 fi
 source ${conf_path}
 
-for v in DISCOURSE_USERNAME DISCOURSE_PASSWORD DISCOURSE_URL DISCOURSE_TO_VISIT; do
-    if [ -z ${!v} ]; then
-	    error "Config variable not set: $v"
-    fi
+for v in DISCOURSE_USERNAME DISCOURSE_PASSWORD DISCOURSE_URL \
+	DISCOURSE_TO_VISIT; do
+	if [ -z ${!v} ]; then
+		error "Config variable not set: $v"
+	fi
 done
 
 set -u
@@ -24,13 +25,29 @@ set -u
 cookies=$(mktemp)
 trap "rm -f ${cookies}" 0
 
-csrf=$(curl -j -b ${cookies} -c ${cookies} "${DISCOURSE_URL}/session/csrf" -H 'X-Requested-With: XMLHttpRequest' --compressed -sfS | sed -ne 's/{"csrf":"\(.\+\)"}/\1/p')
+csrf=$(curl -j -b ${cookies} -c ${cookies} "${DISCOURSE_URL}/session/csrf" \
+	-H 'X-Requested-With: XMLHttpRequest' \
+	--compressed -sfS \
+	| sed -ne 's/{"csrf":"\(.\+\)"}/\1/p')
 if [ -z ${csrf} ]; then
 	error 'Could not get CSRF token!'
 fi
 
-curl -X POST -b ${cookies} -c ${cookies} "${DISCOURSE_URL}/session" -H 'X-Requested-With: XMLHttpRequest' -H "X-CSRF-Token: $csrf" --data-urlencode "login=${DISCOURSE_USERNAME}" --data-urlencode "password=${DISCOURSE_PASSWORD}" --data-urlencode "second_factor_method=1" --compressed -sfS >/dev/null || error 'Could not create session!'
-curl -X POST -b ${cookies} -c ${cookies} "${DISCOURSE_URL}/login" --data-urlencode "login=${DISCOURSE_USERNAME}" --data-urlencode "password=${DISCOURSE_PASSWORD}" --compressed -sfS >/dev/null || error 'Could not sign in the user!'
-curl -b ${cookies} -c ${cookies} "${DISCOURSE_URL}/${DISCOURSE_TO_VISIT}" --compressed -sfS >/dev/null || error 'Could not visit TOPIC-TO-VISIT!'
+curl -X POST -b ${cookies} -c ${cookies} "${DISCOURSE_URL}/session" \
+	-H 'X-Requested-With: XMLHttpRequest' \
+	-H "X-CSRF-Token: $csrf" \
+	--data-urlencode "login=${DISCOURSE_USERNAME}" \
+	--data-urlencode "password=${DISCOURSE_PASSWORD}" \
+	--data-urlencode "second_factor_method=1" \
+	--compressed -sfS >/dev/null \
+	|| error 'Could not create session!'
+curl -X POST -b ${cookies} -c ${cookies} "${DISCOURSE_URL}/login" \
+	--data-urlencode "login=${DISCOURSE_USERNAME}" \
+	--data-urlencode "password=${DISCOURSE_PASSWORD}" \
+	--compressed -sfS >/dev/null \
+	|| error 'Could not sign in the user!'
+curl -b ${cookies} -c ${cookies} "${DISCOURSE_URL}/${DISCOURSE_TO_VISIT}" \
+	--compressed -sfS >/dev/null \
+	|| error 'Could not visit TOPIC-TO-VISIT!'
 
-echo "Visited the website successfully at $(date)" 1>&2
+echo "Visited the website successfully at $(date)"
